0535a8902a2e4135fd0b704ef5d710e83f02cab5
Frankie Robertson <frankie@robertson.name>
2021-08-26 15:39:46 +0300

    Add Alembic configuration

--- selfassess/alembic/env.py
+++ selfassess/alembic/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from selfassess import database
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -15,9 +17,7 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = database.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
