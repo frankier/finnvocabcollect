68dc6151577b9152f073b1bfa7a36cc95523bbf1
Frankie Robertson <frankie@robertson.name>
2021-09-17 12:16:08 +0300

    Add util to write out payment info

--- selfassess/selfassess/export/duckdb.py
+++ selfassess/selfassess/export/duckdb.py
@@ -1,10 +1,8 @@
 import click
 import duckdb
-import orjson
-import user_agents
-from statistics import mode
 from selfassess.utils import get_session
-from selfassess.database import Participant, Response, SessionLogEntry
+from selfassess.database import Participant
+from .utils import get_participant_sessions
 
 
 def setup_duckdb(db_out):
@@ -42,81 +40,6 @@ def setup_duckdb(db_out):
     );
     """)
     return conn
-
-
-SESSION_TIMEOUT = 300
-
-
-def ua_to_device(ua):
-    ua_parse = user_agents.parse(ua)
-    if ua_parse.is_tablet:
-        return "tablet"
-    elif ua_parse.is_mobile:
-        return "mobile"
-    elif ua_parse.is_pc:
-        return "pc"
-    else:
-        return "unknown"
-
-
-def get_participant_sessions(participant):
-    events = []
-    for session_log_entry in participant.session_log_entries:
-        events.append((session_log_entry.timestamp, session_log_entry))
-    for slot in participant.response_slots:
-        latest_timestamp = None
-        responses = []
-        for response in slot.responses:
-            if (latest_timestamp is None
-                    or response.timestamp > latest_timestamp):
-                latest_timestamp = response.timestamp
-            responses.append((response.timestamp, response))
-        for timestamp, response in responses:
-            response.is_latest = timestamp == latest_timestamp
-        events.extend(responses)
-        for presentation in slot.presentations:
-            events.append((presentation.timestamp, presentation))
-    events.sort()
-    last_timestamp = None
-    sessions = []
-
-    def new_session():
-        sessions.append({
-            "has_selfassess": False,
-            "response": [],
-            "devices": [],
-            "first_timestamp": None,
-            "time": None,
-        })
-
-    def end_session(timestamp):
-        sessions[-1]["time"] = (timestamp - sessions[-1]["first_timestamp"])
-        if sessions[-1]["devices"]:
-            device = mode(sessions[-1]["devices"])
-        else:
-            device = "unknown"
-        sessions[-1]["device"] = device
-        sessions[-1]["last_timestamp"] = last_timestamp
-
-    new_session()
-    for timestamp, event in events:
-        if (
-            last_timestamp is not None
-            and (timestamp - last_timestamp).total_seconds() > SESSION_TIMEOUT
-        ):
-            end_session(last_timestamp)
-            new_session()
-        if sessions[-1]["first_timestamp"] is None:
-            sessions[-1]["first_timestamp"] = timestamp
-        if isinstance(event, Response):
-            sessions[-1]["has_selfassess"] = True
-            sessions[-1]["response"].append(event)
-        elif isinstance(event, SessionLogEntry):
-            device = ua_to_device(orjson.loads(event.payload)["user_agent"])
-            sessions[-1]["devices"].append(device)
-        last_timestamp = timestamp
-    end_session(timestamp)
-    return sessions
 
 
 @click.command()
---
+++ selfassess/selfassess/export/payment.py
@@ -0,0 +1,50 @@
+import click
+from pandas import DataFrame
+
+from selfassess.utils import get_session
+from selfassess.database import Participant
+from .utils import get_participant_sessions
+
+
+@click.command()
+@click.argument("csvout", type=click.Path())
+def main(csvout):
+    """
+    Given name
+
+    Surname
+
+    E-mail address
+
+    Period of the task to be done
+    """
+    sqlite_sess = get_session()
+    participants = sqlite_sess.query(Participant)
+    names = []
+    surnames = []
+    email = []
+    period = []
+    for participant in participants:
+        names.append(participant.given_name)
+        surnames.append(participant.surname)
+        email.append(participant.email)
+        time = (
+            sum((
+                part_session["time"].total_seconds()
+                for part_session
+                in get_participant_sessions(participant)
+            ))
+            + 60 * 30
+        ) / (60 * 60)
+        period.append(int(time * 4 + 0.5) / 4)
+    df = DataFrame({
+        "name": names,
+        "surname": surnames,
+        "email": email,
+        "period": period,
+    })
+    df.to_csv(csvout, float_format='%.2f')
+
+
+if __name__ == "__main__":
+    main()
