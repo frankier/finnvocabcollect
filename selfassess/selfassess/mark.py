398ac636eba6aae2501d06450ea5e996716aeb02
Frankie Robertson <frankie@robertson.name>
2021-09-17 18:08:27 +0300

    Add script for marking miniexam

---
+++ selfassess/selfassess/mark.py
@@ -0,0 +1,31 @@
+import click
+
+from .database import Participant
+from .utils import get_session
+
+
+@click.command()
+@click.argument("email")
+def main(email):
+    session = get_session()
+    participant = (
+        session.query(Participant).filter(email=email).scalars().first()
+    )
+    for miniexam_slot in participant.miniexam_slots:
+        resp = miniexam_slot.responses[-1]
+        while 1:
+            mark = input("Word: {}; Language: {}; Type: {}; Response: {} (1/2/3) > ".format(
+                miniexam_slot.word.word,
+                resp.response_lang.name(),
+                resp.response_type.name(),
+                resp.response
+            ))
+            mark = int(mark)
+            if mark in (1, 2, 3):
+                resp.mark = mark
+                session.commit()
+                break
+
+
+if __name__ == "__main__":
+    main()
