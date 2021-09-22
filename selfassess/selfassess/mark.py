a5642a784ec3fea5ee365df0faa34eda8c1b6b09
Frankie Robertson <frankie@robertson.name>
2021-09-22 13:25:40 +0300

    Fix marking to use 5 point scale

--- selfassess/selfassess/mark.py
+++ selfassess/selfassess/mark.py
@@ -14,14 +14,14 @@ def main(email):
     for miniexam_slot in participant.miniexam_slots:
         resp = miniexam_slot.responses[-1]
         while 1:
-            mark = input("Word: {}; Language: {}; Type: {}; Response: {} (1/2/3) > ".format(
+            mark = input("Word: {}; Language: {}; Type: {}; Response: {} (1-5) > ".format(
                 miniexam_slot.word.word,
                 resp.response_lang.name(),
                 resp.response_type.name(),
                 resp.response
             ))
             mark = int(mark)
-            if mark in (1, 2, 3):
+            if mark in range(1, 6):
                 resp.mark = mark
                 session.commit()
                 break
