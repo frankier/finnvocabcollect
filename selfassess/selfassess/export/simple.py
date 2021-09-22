51febd328e7e82b0355f9ba3f3c1394d509004c0
Frankie Robertson <frankie@robertson.name>
2021-09-22 13:26:15 +0300

    Add possibility of including session in simple export

--- selfassess/selfassess/export/simple.py
+++ selfassess/selfassess/export/simple.py
@@ -1,20 +1,25 @@
 import click
 import duckdb
-import os
 
 
 @click.command()
 @click.argument("db_in")
 @click.argument("tsv_out")
-def main(db_in, tsv_out):
+@click.option("--include-session/--no-include-session")
+def main(db_in, tsv_out, include_session):
     conn = duckdb.connect(db_in)
+    if include_session:
+        session_col = "selfassess_response.session_id,\n"
+    else:
+        session_col = ""
     conn.execute(f"""
     copy (
         select
             participant.id as participant,
+            {session_col}
             selfassess_response.word,
             selfassess_response.time_secs,
-            selfassess_response.grade
+            selfassess_response.rating
         from
             selfassess_response
             join selfassess_session
