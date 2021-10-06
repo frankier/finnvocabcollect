c0288340babb38245e69c43a0f582d035528cc0c
Frankie Robertson <frankie@robertson.name>
2021-10-06 21:31:35 +0300

    Modify plot_selfassess to make separate plot for each participant

--- plot_selfassess.py
+++ plot_selfassess.py
@@ -2,6 +2,8 @@ import click
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
+from os.path import join as pjoin
+from os import makedirs
 
 from freqknowfit.nonparametric.transfer import draw_plot
 from finnwordlist.multicorpusfreqs import REL_FREQ_COLS
@@ -23,14 +25,16 @@ def main(wordlist_inf, know_inf, figout):
     know_df = pandas.read_csv(know_inf)
     know_df["score"] = know_df["rating"]
     know_df = know_df.join(wordlist_df.set_index("lemma"), "word")
-    # TODO: modify freqknowfit to take column names
-    fig, axs = plt.subplots(3, 3, sharex=True, sharey=True)
-    fig.set_size_inches(12, 12)
-    for ax, col in zip(axs.flatten(), COLS):
-        know_df["zipf"] = np.log10(know_df[col] * 1000)
-        draw_plot(know_df, ax, create_fit=False, ordinal=True, bw=bw)
-    plt.tight_layout()
-    fig.savefig(figout)
+    makedirs(figout, exist_ok=True)
+    for participant, group_df in know_df.groupby("participant"):
+        # TODO: modify freqknowfit to take column names
+        fig, axs = plt.subplots(3, 3, sharex=True, sharey=True)
+        fig.set_size_inches(12, 12)
+        for ax, col in zip(axs.flatten(), COLS):
+            group_df["zipf"] = np.log10(group_df[col] * 1000)
+            draw_plot(group_df, ax, create_fit=False, ordinal=True, bw=bw)
+        plt.tight_layout()
+        fig.savefig(pjoin(figout, f"{participant}.png"))
 
 
 if __name__ == "__main__":
