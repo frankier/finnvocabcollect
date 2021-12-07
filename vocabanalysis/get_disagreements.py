90f5b69023ad3eb2ca5b0565116b43397ed51657
Frankie Robertson <frankie@robertson.name>
2021-12-07 11:08:21 +0200

    Put labels on confusion matrix

--- vocabanalysis/get_disagreements.py
+++ vocabanalysis/get_disagreements.py
@@ -1,7 +1,8 @@
 import click
 import pandas
 
-from sklearn.metrics import confusion_matrix, cohen_kappa_score
+from sklearn.metrics import confusion_matrix, cohen_kappa_score, ConfusionMatrixDisplay
+from matplotlib import pyplot as mpl
 
 
 def clean_mark(col):
@@ -11,7 +12,8 @@ def clean_mark(col):
 @click.command()
 @click.argument("annotation_a", type=click.File("r"))
 @click.argument("annotation_b", type=click.File("r"))
-def main(annotation_a, annotation_b):
+@click.argument("figout", type=click.Path(), required=False)
+def main(annotation_a, annotation_b, figout):
     df_a = pandas.read_csv(annotation_a, sep="\t")
     df_b = pandas.read_csv(annotation_b, sep="\t")
     df_joined = pandas.merge(
@@ -19,6 +21,8 @@ def main(annotation_a, annotation_b):
         df_b.drop(columns=["participant_id", "word", "lang", "type", "resp"]),
         on="response_id"
     )
+    missing = df_joined[df_joined[["mark_x", "mark_y"]].isna().any(axis=1)]
+    print(missing)
     df_clean = df_joined[["mark_x", "mark_y"]].dropna()
     df_clean["mark_x"] = clean_mark(df_clean["mark_x"])
     df_clean["mark_y"] = clean_mark(df_clean["mark_y"])
@@ -35,8 +39,12 @@ def main(annotation_a, annotation_b):
         )
     )
     print("Confusion matrix")
-    print(confusion_matrix(df_clean["mark_x"], df_clean["mark_y"]))
+    confmat = confusion_matrix(df_clean["mark_x"], df_clean["mark_y"])
     print(df_joined[df_joined["mark_x"] != df_joined["mark_y"]])
+    ConfusionMatrixDisplay(confmat, display_labels=range(1, 6)).plot()
+    mpl.ylabel("Annotator 1")
+    mpl.xlabel("Annotator 2")
+    mpl.savefig(figout)
 
 
 if __name__ == "__main__":
